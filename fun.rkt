f0e2e3785f7dd8953a3de2cc70759c609ea8569b
berukaimerito <kocakberkaymert@gmail.com>
2024-10-18 13:15:45 +0200

    fibonacci sequence in lisp

---
+++ fun.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname fun) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(+ 3 4)
+
+(+ 4 (* 2 4))
+
+(/ 12 (* 2 3)) ; <primitive> <expression>
+
+(sqr 3)
+
+(sqrt 16)
+
+(sqrt (+(sqr 3)(sqr 4)))
+
+(/ (+ 4 6.2 -12) 3)
+
+0.6
+
+(sqrt 2) ; #i1.414
--- fundamentals.lisp
+++ fundamentals.lisp
@@ -1,13 +1,39 @@
 (require 'asdf)
 (require 'uiop)
 
-(defun make-sqr (col)
+(defun make-cube (col)
     (mapcar (lambda (x) (expt x 3)) col))
 
-(make-sqr '(1 2 3 4))
+(make-cube '(1 2 3 4))
 
 (defun hello ()
     "say hello to USER"
-    (format t "hello ~a" (uiop:getenv "USER")))
+    (format t "hello ~a%" (uiop:getenv "USER")))
 
 (hello)
+
+(defun stdio (string-like)
+    (format t "~a~%" string-like))
+
+(stdio 23249)
+
+(defun math-low (x y z)
+    (let ((res (+ x z)))
+    (- y res)))
+
+
+(math-low 2 7 3) ;; should return 2
+
+
+(defun fibonacci (n)
+    (if (<= n 1)   
+    n 
+    (+ (fibonacci (- n 1))
+       (fibonacci (- n 2)))))
+    
+(defun print-fibonacci-sequence (n)
+    (dotimes (i (+ n 1))
+        (format t "~a " (fibonacci i))))
+
+        
+(print-fibonacci-sequence 7)
